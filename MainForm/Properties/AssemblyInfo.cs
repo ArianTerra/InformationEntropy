d87c3be543a34f1183f734305b854c3bcea26c13
a.serediuk <a.serediuk@student.csn.khai.edu>
2020-09-24 19:12:33 +0300

    Release commit

--- MainForm/Properties/AssemblyInfo.cs
+++ MainForm/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MainForm")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Text Entropy")]
+[assembly: AssemblyDescription("Lab1 Serediuk Mykhailenko Demchenko")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("KhAI 535В")]
 [assembly: AssemblyProduct("MainForm")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
