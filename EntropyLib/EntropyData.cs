7f4935f9c8408d2ab0084f509f97c7708c4806e3
Mykyta <m.mykhailenko@student.csn.khai.edu>
2020-09-25 10:13:35 +0300

    Changed base. Now works with bits.

--- EntropyLib/EntropyData.cs
+++ EntropyLib/EntropyData.cs
@@ -42,8 +42,9 @@ namespace EntropyLib
             {
                 dictionary[key] /= Convert.ToDouble(temptext.Length);
             }
+
             // Расчет по формуле Шеннона
-            double shennon = -dictionary.Count * dictionary.Values.Aggregate(0.0, (s, x) => s + x * Math.Log(x));
+            double shennon = -dictionary.Count * dictionary.Values.Aggregate(0.0, (s, x) => s + x * Math.Log(x, 2));
             // Расчет по формуле Хартли
             double hartly = Math.Log(dictionary.Keys.Count);
 
